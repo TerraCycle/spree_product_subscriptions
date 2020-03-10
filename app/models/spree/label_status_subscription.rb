de7e6a68d2dbf24424bcc5b005b2fd519d8397f5
Janos Seller <seller.janos@gmail.com>
2020-03-10 16:23:07 +0100

    Change label to package to work with the current state of the shop

---
+++ app/models/spree/label_status_subscription.rb
@@ -0,0 +1,24 @@
+module Spree
+  class LabelStatusSubscription < Spree::Subscription
+    alias_attribute :frequency, :label_status
+
+    with_options presence: true do
+      validates :label_status
+    end
+
+    def process
+      new_order = recreate_order
+    end
+
+    def can_be_proccesed?
+      previous_order = orders.any? ? orders.last : parent_order
+      order_status_correct?(previous_order)
+    end
+
+    private
+
+    def order_status_correct?(order)
+      order.has_inbound_package? && order.inbound_packages_status_is?(label_status.label_status)
+    end
+  end
+end
