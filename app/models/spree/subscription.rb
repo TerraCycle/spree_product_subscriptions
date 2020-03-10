190abba3c548962b89fdc037df7769735c073cca
Janos Seller <seller.janos@gmail.com>
2020-03-10 16:23:57 +0100

    update gem to spree 4

--- app/models/spree/subscription.rb
+++ app/models/spree/subscription.rb
@@ -140,7 +140,7 @@ module Spree
       end
 
       def make_new_order
-        orders.create(order_attributes)
+        orders.create!(order_attributes)
       end
 
       def add_variant_to_order(order)
