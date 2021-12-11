b9d2f7cbded8316c6f55e925ef052a47b05c2a01
Erik Csonka <erik.csonka@terracycle.com>
2021-12-11 11:34:28 +0100

    Add locale param to routes

--- app/overrides/add_subscription_frequency_in_configurations.rb
+++ app/overrides/add_subscription_frequency_in_configurations.rb
@@ -3,6 +3,6 @@ Deface::Override.new(
   name: 'add_subscription_frequency_in_configurations',
   insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
   text: %q{
-    <%= configurations_sidebar_menu_item(Spree.t(:subscription_frequencies), admin_subscription_frequencies_path) %>
+    <%= configurations_sidebar_menu_item(Spree.t(:subscription_frequencies), admin_subscription_frequencies_path(params[:locale])) %>
     }
 )
