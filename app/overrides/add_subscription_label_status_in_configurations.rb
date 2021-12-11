fd550d860be3929379242846e063d72653633df7
Erik Csonka <erik.csonka@terracycle.com>
2021-12-11 11:34:28 +0100

    Add locale param to routes

--- app/overrides/add_subscription_label_status_in_configurations.rb
+++ app/overrides/add_subscription_label_status_in_configurations.rb
@@ -3,6 +3,6 @@ Deface::Override.new(
   name: 'add_subscription_label_status_in_configurations',
   insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
   text: %q{
-    <%= configurations_sidebar_menu_item(Spree.t(:subscription_label_statuses), admin_subscription_label_statuses_path) %>
+    <%= configurations_sidebar_menu_item(Spree.t(:subscription_label_statuses), admin_subscription_label_statuses_path(params[:locale])) %>
     }
 )
